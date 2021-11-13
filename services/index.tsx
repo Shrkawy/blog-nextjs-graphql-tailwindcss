828485e07b54f13493917e09c843e6e504fcdd22
Sharkawy <mahmoud.alshrkawy@gmail.com>
2021-11-13 05:45:41 +0200

    add getRecentPosts query

--- services/index.tsx
+++ services/index.tsx
@@ -1,39 +1,4 @@
-import { request, gql } from "graphql-request";
+import { getPosts } from "./getPosts";
+import { getRecentPosts } from "./getRecentPosts";
 
-const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
-
-export const getPosts = async () => {
-  const query = gql`
-    query MyQuery {
-      postsConnection {
-        edges {
-          node {
-            author {
-              bio
-              name
-              id
-              photo {
-                url
-              }
-            }
-            createdAt
-            slug
-            title
-            excerpt
-            featuredImage {
-              url
-            }
-            categories {
-              name
-              slug
-            }
-          }
-        }
-      }
-    }
-  `;
-
-  const result = await request(graphqlAPI, query);
-
-  return result.postsConnection.edges;
-};
+export { getPosts, getRecentPosts };
