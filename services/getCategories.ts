055600df51602499eb0a20852964a6a0387dd98c
Sharkawy <mahmoud.alshrkawy@gmail.com>
2021-11-13 06:38:30 +0200

    add getCategories query

---
+++ services/getCategories.ts
@@ -0,0 +1,18 @@
+import { request, gql } from "graphql-request";
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export const getCategories = async () => {
+  const query = gql`
+    query getCategories {
+        categories: {
+            name
+            slug
+        }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.categories;
+};
