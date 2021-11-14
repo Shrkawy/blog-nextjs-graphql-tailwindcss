5e7e69accb01ef0987a11948e6c90d7a79bfc676
Sharkawy <mahmoud.alshrkawy@gmail.com>
2021-11-14 15:00:48 +0200

    fix deplyment

--- pages/Home/components/index.tsx
+++ pages/Home/components/index.tsx
@@ -1,3 +1,1 @@
 export { default as PostCard } from "../../../components/shared/PostCard";
-export { default as PostWidget } from "../../../components/shared/PostWidget";
-export { default as Categories } from "../../../components/shared/Categories";
